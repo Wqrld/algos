0168a5d1ceef1633b16faa7b30fd005ce73166c3
Wqrld <wereld03@gmail.com>
2022-09-13 18:08:34 +0200

    Create quicksort.py

---
+++ python/quicksort.py
@@ -0,0 +1,21 @@
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] < pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+    return i + 1
+
+
+# quicksort
+def quicksort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quicksort(array, low, pi - 1)
+        quicksort(array, pi + 1, high)
+
+
+arr = [5, 4, 2, 1]
+quicksort(arr, 0, len(arr) - 1)
